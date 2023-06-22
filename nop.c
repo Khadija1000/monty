90a79a704e0ff7fad44a056d22b9252fe1c5ed01
asakawa01 <arybou.oualid@gmail.com>
2023-06-22 13:30:18 -0700

    task

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * f_nop- doesn’t do anything
+  *@head: stack's head
+  *@counter: line number
+  *Return: void
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - function that print the top of the stack
+ * @head: stack's head
+ * @counter: line number
+ * Return: void
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
